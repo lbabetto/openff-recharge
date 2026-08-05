93efe055c0f0f1334b88667a719ebf728e03aaae
Luca Babetto <l.babetto@cineca.it>
2026-08-05 16:13:22 +0200

    Add script for pitagora

---
+++ examples/parameter-training/numpy/pitagora.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -j openff_train
+#SBATCH -a cin_staff
+#SBATCH -p dcgp_fua_prod 
+#SBATCH -t 0-04:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+
+source /pitagora_work/cin_staff/lbabetto/miniforge3/etc/profile.d/conda.sh
+conda activate openff-recharge
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+python train-bcc-parameters.py spice-pubchem.smi SMARTS.txt
