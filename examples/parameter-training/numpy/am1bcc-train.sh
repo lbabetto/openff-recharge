0976751d456bed7d96eefa1a30d6db8ce7bf3e84
Luca <l.babetto@cineca.it>
2026-08-06 12:34:00 +0200

    Update script walltime and ncores

--- examples/parameter-training/numpy/am1bcc-train.sh
+++ examples/parameter-training/numpy/am1bcc-train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account cin_staff
 #SBATCH --partition g100_usr_prod 
-#SBATCH --time 0-04:00:00
+#SBATCH --time 1-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=48
 
 source /g100_work/cin_staff/lbabetto/miniforge3/etc/profile.d/conda.sh
 conda activate openff-recharge
