c87b5832720fa72f66c8ec5747f178a7278d68ea
Luca <l.babetto@cineca.it>
2026-08-07 08:54:25 +0200

    Add timing to slurm script + error redirection

--- examples/parameter-training/numpy/am1bcc-train.sh
+++ examples/parameter-training/numpy/am1bcc-train.sh
@@ -2,9 +2,11 @@
 #SBATCH --account cin_staff
 #SBATCH --partition g100_usr_prod 
 #SBATCH --time 1-00:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=48
+#SBATCH --nodes 1
+#SBATCH --output slurm-%j.out
+#SBATCH --error slurm-%j.err
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 48
 
 source /g100_work/cin_staff/lbabetto/miniforge3/etc/profile.d/conda.sh
 conda activate openff-recharge
@@ -12,4 +14,4 @@ conda activate openff-recharge
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-python train-bcc-parameters.py $1 $2
+time python train-bcc-parameters.py $1 $2
