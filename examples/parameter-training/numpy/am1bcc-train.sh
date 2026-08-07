cdbaceb4a111b01a530f5defa883ac7cdc6b7114
Luca <l.babetto@cineca.it>
2026-08-07 08:58:26 +0200

    Update SLURM script

--- examples/parameter-training/numpy/am1bcc-train.sh
+++ examples/parameter-training/numpy/am1bcc-train.sh
@@ -3,10 +3,12 @@
 #SBATCH --partition g100_usr_prod 
 #SBATCH --time 1-00:00:00
 #SBATCH --nodes 1
-#SBATCH --output slurm-%j.out
-#SBATCH --error slurm-%j.err
+#SBATCH --output slurm-%x-%j.out
+#SBATCH --error slurm-%x-%j.err
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 48
+#SBATCH --mail-user l.babetto@cineca.it
+#SBATCH --mail-type ALL
 
 source /g100_work/cin_staff/lbabetto/miniforge3/etc/profile.d/conda.sh
 conda activate openff-recharge
